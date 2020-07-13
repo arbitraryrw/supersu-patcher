4e445cf8bbb3427b9043edac7ecbc07efc44b169
arbitraryrw <nickcucakovic@gmail.com>
2020-07-13 22:03:29 +0100

    utils: adding zip functionality to repackage artifacts post processing

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,7 +8,7 @@ class Utils:
 
     def __init__(self):
         pass
-    
+
     def delete_dir_tree(self, dir_path):
         if os.path.isdir(dir_path):
             try:
@@ -27,12 +27,43 @@ class Utils:
         else:
             pass
 
-    def unzip(self, archive_path, unzip_dir_path):
+    def unzip_archive(self, archive_path, unzip_dir_path):
         if os.path.isfile(unzip_dir_path) or os.path.isdir(unzip_dir_path):
             exit(f"[ERROR] file {unzip_dir_path} already exists")
 
         with zipfile.ZipFile(archive_path,"r") as zip_ref:
             zip_ref.extractall(unzip_dir_path)
+    
+    def zip_archive(self, dir_to_zip_path, output_dir_path):
+
+        files_to_zip = self.get_files_in_dir(dir_to_zip_path)
+
+        with zipfile.ZipFile(output_dir_path,'w') as zip: 
+            for file in files_to_zip: 
+                # Write the file to the zip but remove the abs path
+                zip.write(file, file.replace(dir_to_zip_path,"")) 
+        
+        if not os.path.isfile(output_dir_path):
+            exit(f"[ERROR] unable to copy file {dir_to_zip_path} to {output_dir_path}")
+
+    def get_files_in_dir(self, dir_path):
+        file_paths = [] 
+    
+        # crawling through directory and subdirectories 
+        for root, dirs, files in os.walk(dir_path): 
+            for filename in files: 
+                # join the two strings in order to form the full filepath. 
+                filepath = os.path.join(root, filename) 
+                file_paths.append(filepath) 
+    
+        # returning all file paths 
+        return file_paths 
+
+    def copy_file(self, src_file, dest_file):
+        shutil.copyfile(src_file,dest_file)
+
+        if not os.path.isfile(dest_file):
+            exit(f"[ERROR] unable to copy file {src_file} to {dest_file}")
 
             
         
